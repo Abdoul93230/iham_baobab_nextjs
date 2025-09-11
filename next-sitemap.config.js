bad2e66bb67d511f0c97aeb326dba49dd2d138c5
Abdoul93230 <abdoulrazak9323@gmail.com>
2025-09-11 19:54:31 +0100

    updated auther page

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://www.ihambaobab.com/', // remplace par ton domaine
+  generateRobotsTxt: true, // génère aussi un robots.txt
+  sitemapSize: 7000,
+};
